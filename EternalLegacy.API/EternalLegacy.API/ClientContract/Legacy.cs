63bcc1ac005b6a326c2a45cf0fb3c9a05cd79d37
Justin Morrow <Justin.Morrow@kiewit.com>
2023-09-23 22:18:28 -0500

    data model changes

--- EternalLegacy.API/EternalLegacy.API/ClientContract/Legacy.cs
+++ EternalLegacy.API/EternalLegacy.API/ClientContract/Legacy.cs
@@ -5,8 +5,9 @@ namespace EternalLegacy.API.ClientContract
     public class Legacy
     {
         public int LegacyId { get; set; }
+        public string Name { get; set; }
         public LegacyType LegacyType { get; set; }
         public bool IsPublished { get; set; }
-        public DateTime OpenDate { get; set; }
+        public DateTime? OpenDate { get; set; }
     }
 }
